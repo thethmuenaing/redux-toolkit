ad524a6dbb2ec46414222877534d838c25031548
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-09-25 08:49:00 +0630

    adding some code and layerout of app

---
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CakeView = () => {
+	return (
+		<div>
+			<h2>Number of Cake - </h2>
+			<button>Order cake</button>
+			<span> </span>
+			<button>Restock cakes</button>
+		</div>
+	);
+};
+
+export default CakeView;
---
+++ react-rtk-demo/src/features/icecream/IceCreamView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const IceCreamView = () => {
+	return (
+		<div>
+			<h2>Number of ice creams - </h2>
+			<button>Order ice cream</button>
+			<span> </span>
+			<button>Restock ice creams</button>
+		</div>
+	);
+};
+
+export default IceCreamView;
