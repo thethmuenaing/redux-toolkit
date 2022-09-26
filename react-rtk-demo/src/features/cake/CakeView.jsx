b482a735a3f2a1405ca2b23442c8c60f8adb1d23
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-09-26 15:35:56 +0630

    add the useDispatch function and buid the local storage

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./cakeSlice";
 
 const CakeView = () => {
 	const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+	const dispatch = useDispatch();
 	return (
 		<div>
 			<h2>Number of Cake - {numOfCakes}</h2>
-			<button>Order cake</button>
+			<button
+				onClick={() => {
+					dispatch(ordered());
+				}}
+			>
+				Order cake
+			</button>
 			<span> </span>
-			<button>Restock cakes</button>
+			<button
+				onClick={() => {
+					dispatch(restocked(3));
+				}}
+			>
+				Restock cakes
+			</button>
 		</div>
 	);
 };
--- react-rtk-demo/src/features/icecream/IceCreamView.jsx
+++ react-rtk-demo/src/features/icecream/IceCreamView.jsx
@@ -1,14 +1,38 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./icecreamSlice";
 
 const IceCreamView = () => {
+	const [value, setValue] = useState(1);
 	const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+	const dispatch = useDispatch();
 	return (
 		<div>
 			<h2>Number of ice creams - {numOfIcecreams}</h2>
-			<button>Order ice cream</button>
+
+			<button
+				onClick={() => {
+					dispatch(ordered());
+				}}
+			>
+				Order ice cream
+			</button>
 			<span> </span>
-			<button>Restock ice creams</button>
+			<input
+				type="number"
+				value={value}
+				onChange={(e) => {
+					setValue(parseInt(e.target.value));
+				}}
+			/>
+			<span> </span>
+			<button
+				onClick={() => {
+					dispatch(restocked(value));
+				}}
+			>
+				Restock ice creams
+			</button>
 		</div>
 	);
 };
