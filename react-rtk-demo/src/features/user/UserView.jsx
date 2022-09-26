23674576d0e1d92ce9b42681ee685ab79c041f47
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-09-26 23:19:52 +0630

    add the fetching datat from placeholder

--- react-rtk-demo/src/features/user/UserView.jsx
+++ react-rtk-demo/src/features/user/UserView.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
 
 const UserView = () => {
+	const user = useSelector((state) => state.user);
+	console.log(user);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, []);
 	return (
 		<div>
 			<h2>List of Users</h2>
+			{user.loading && <div>Loading...</div>}
+			{!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+			{!user.loading && user.users.length ? (
+				<ul>
+					{user.users.map((user) => (
+						<li key={user.id}>{user.name}</li>
+					))}
+				</ul>
+			) : null}
 		</div>
 	);
 };
